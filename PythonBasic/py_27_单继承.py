a9bb85d242ba240d0e6d4957939be695628d139b
shangguanxiaoguan <1414122327@qq.com>
2020-03-29 21:00:48 +0800

    单继承

---
+++ PythonBasic/py_27_单继承.py
@@ -0,0 +1,57 @@
+"""
+1.继承的概念：子类拥有父类的所有方法和属性
+2.继承的语法：
+   class 类名(父类名)：
+      pass
+3.子类继承自父类，可以直接享受父类中已经封装好的方法，不需要再次开发
+  子类中应该根据职责，封装子类持有的属性和方法
+
+4.专业术语：
+  Dog类是Animal类的子类，Animal类是DOg类的父类，Dog类从Animal类继承
+  Dog类是Animal类的派生类，Animal类是Dog类的基类，Dog类从Animal类派生
+
+"""
+
+
+class Animal:
+    def eat(self):
+        print("吃")
+
+    def drink(self):
+        print("喝")
+
+    def run(self):
+        print("跑")
+
+    def sleep(self):
+        print("睡")
+
+
+class Dog(Animal):
+    # def eat(self):
+    #     print("吃")
+    #
+    # def drink(self):
+    #     print("喝")
+    #
+    # def run(self):
+    #     print("跑")
+    #
+    # def sleep(self):
+    #     print("睡")
+
+    def bark(self):
+        print("叫")
+
+
+# 创建对象
+# wangcai = Animal()
+wangcai = Dog()
+wangcai.eat()
+wangcai.drink()
+wangcai.run()
+wangcai.sleep()
+wangcai.bark()
+
+# 第33节
+
